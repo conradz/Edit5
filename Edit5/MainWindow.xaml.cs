f6d3bdc9dc508c2d8141e6a32d1b7a5c67245f48
Conrad <conrad@abcsoftek.com>
2011-09-13 07:33:21 -0400

    Initial commit

---
+++ Edit5/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IronJS;
+using IronJS.Hosting;
+
+namespace Edit5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var c = new IronJS.Hosting.CSharp.Context();
+            c.SetGlobal("Test", "MyValue");
+            this.Title = c.Execute("Test").ToString();
+        }
+    }
+}
